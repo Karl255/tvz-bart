d8263a4a22f24e523846ec118c850febbac7c8ad
Karl255 <49595438+Karl255@users.noreply.github.com>
2022-09-29 22:51:15 +0200

    Styled info panels

--- src/lib/ClassPeriodInfo.svelte
+++ src/lib/ClassPeriodInfo.svelte
@@ -5,26 +5,29 @@
 </script>
 
 {#if classPeriod}
-	<p><b>ID:</b> {classPeriod.id}</p>
-	<p><b>Color:</b> {classPeriod.apiColor}</p>
+	<!-- <p><b>ID:</b> {classPeriod.id}</p> -->
+	<!-- <p><b>Color:</b> {classPeriod.apiColor}</p> -->
 	<p>{classPeriod.start.toString({ smallestUnit: "minutes" })} - {classPeriod.end.toString({ smallestUnit: "minutes" })}</p>
-	<p><b>{classPeriod.courseName}</b></p>
-	<p>{classPeriod.className}</p>
+	<p><strong>{classPeriod.className}</strong></p>
 	<p>{classPeriod.classType}</p>
-	<p>{classPeriod.professor}</p>
 	<p>{classPeriod.classroom}</p>
-	{#if classPeriod.amountOfStudents}
-		<p>{classPeriod.amountOfStudents} upisanih studenata.</p>
+
+	{#if classPeriod.group || classPeriod.note}
+		<br>
 	{/if}
+
 	{#if classPeriod.group}
 		<p><b>Grupa:</b> {classPeriod.group}</p>
 	{/if}
+
 	{#if classPeriod.note}
 		<p><b>Napomena:</b> {classPeriod.note}</p>
 	{/if}
+
+	<br>
+
+	<p>{classPeriod.professor}</p>
+	<p><strong>{classPeriod.courseName}</strong></p>
 {:else}
 	<slot />
 {/if}
-
-<style lang="scss">
-</style>
