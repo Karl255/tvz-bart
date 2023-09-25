bafe931bb8779d16ea4090f3c742214faf5b9460
Karl255 <49595438+Karl255@users.noreply.github.com>
2023-09-25 19:30:12 +0200

    Added link to repo

--- src/lib/components/TemporaryNavigation.svelte
+++ src/lib/components/TemporaryNavigation.svelte
@@ -4,4 +4,6 @@
 		<li><a href="/">Standard schedule</a></li>
 		<li><a href="/custom">Custom schedules</a></li>
 	</ul>
+
+	<p>You can find the source code in the <a href="https://github.com/Karl255/tvz-bart">GitHub repo</a>.</p>
 </nav>
